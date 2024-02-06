f19e49b5bf6c7862c69292ea1a1f3e1ac127d2f8
ScottShadow <rudaseswascottmc@gmail.com>
2024-02-06 14:17:38 +0000

    initial commit

---
+++ models/base_model.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""
+
+"""
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+    def __init__(self, *arg, **kwargs):
+        time_iso_format = "%Y-%m-%dT%H:%M:%S.%f"
+        if (kwargs):
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue
+                elif key == "created_at" or key == "updated_at":
+                    setattr(self, key, datetime.strptime(
+                        value, time_iso_format))
+                else:
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+
+            self.created_at = datetime.utcnow()
+            self.updated_at = datetime.utcnow()
+
+    def save(self):
+        """
+        """
+        self.updated_at = datetime.utcnow()
+
+    def to_dict(self):
+        """
+        """
+        my_dict = self.__dict__.copy()
+        my_dict["__class__"] = self.__class__.__name__
+        my_dict["created_at"] = self.created_at.isoformat()
+        my_dict["updated_at"] = self.updated_at.isoformat()
+
+        return my_dict
+
+    def __str__(self):
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
+
+if __name__ == "__main__":
+    my_model = BaseModel()
+    my_model.name = "First Model"
+    my_model.my_number = 66
+    print(my_model)
+    my_model.save()
+    print(my_model)
+    my_model_json = my_model.to_dict()
+    print(my_model_json)

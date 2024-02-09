2a46cf92de4075d213fc61c5ce3f864a57bc798d
ScottShadow <rudaseswascottmc@gmail.com>
2024-02-09 14:10:54 +0000

    added files

--- models/base_model.py
+++ models/base_model.py
@@ -4,6 +4,7 @@
 """
 import uuid
 from datetime import datetime
+import models
 
 
 class BaseModel:
@@ -23,20 +24,22 @@ class BaseModel:
 
             self.created_at = datetime.utcnow()
             self.updated_at = datetime.utcnow()
+        models.storage.new(self)
 
     def save(self):
         """
         """
         self.updated_at = datetime.utcnow()
+        models.storage.save()
 
     def to_dict(self):
         """
         """
+
         my_dict = self.__dict__.copy()
         my_dict["__class__"] = self.__class__.__name__
         my_dict["created_at"] = self.created_at.isoformat()
         my_dict["updated_at"] = self.updated_at.isoformat()
-
         return my_dict
 
     def __str__(self):
---
+++ models/engine/file_storage.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""
+
+"""
+import json
+import os
+from models.base_model import BaseModel
+
+
+class FileStorage:
+    """
+
+    """
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """ returns the dictionary __objects"""
+        return FileStorage.__objects
+
+    def new(self, obj):
+        """sets in __objects the obj with key <obj class name>.id"""
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        FileStorage.__objects[key] = obj
+
+    def save(self):
+        """serializes __objects to the JSON file (path: __file_path)"""
+        objs = FileStorage.__objects
+        obj_dict = {}
+        for obj in objs.keys():
+            obj_dict[obj] = objs[obj].to_dict()
+        with open(FileStorage.__file_path, "w", encoding="utf-8") as file:
+            json.dump(obj_dict, file)
+
+    def reload(self):
+        """deserializes the JSON file to __objects (only if the JSON file (__file_path) exists ;
+          otherwise, do nothing. If the file doesn’t exist, no exception should be raised)"""
+        with open(FileStorage.__file_path, 'r', encoding='utf-8') as file:
+            try:
+                obj_dict = json.loads(file)
+                for key, value in obj_dict.items():
+                    class_name, obj_id = key.split(".")
+                    cls = eval(class_name)
+                    obj = cls(**value)
+                    FileStorage.__objects[key] = obj
+            except Exception:
+                pass

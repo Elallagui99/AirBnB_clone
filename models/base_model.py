24f0d81645a33f61c70906d81cff9543ac68c48f
Elallagui99 <got.famme@gmail.com>
2024-02-10 21:57:19 +0100

    add storage

--- models/base_model.py
+++ models/base_model.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
 """
-
+contains class BaseModel
 """
 import uuid
 from datetime import datetime
+import models
 
 
 class BaseModel:
+    """ BaseModel class from which future classes will inherite """
     def __init__(self, *arg, **kwargs):
+        """ initialization """
         time_iso_format = "%Y-%m-%dT%H:%M:%S.%f"
         if (kwargs):
             for key, value in kwargs.items():
@@ -25,13 +28,13 @@ class BaseModel:
             self.updated_at = datetime.utcnow()
 
     def save(self):
-        """
-        """
+        """update attribute with current datetime """
         self.updated_at = datetime.utcnow()
+        models.storage.new(self)
+        models.storage.save()
 
     def to_dict(self):
-        """
-        """
+        """return a dictinary contains all keys and values on instance """
         my_dict = self.__dict__.copy()
         my_dict["__class__"] = self.__class__.__name__
         my_dict["created_at"] = self.created_at.isoformat()
@@ -40,8 +43,12 @@ class BaseModel:
         return my_dict
 
     def __str__(self):
+        """ string representation of basemodel class """
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
+    def delete(self):
+        """ delete the current instance from the storage """
+        models.storage.delete(self)
 
 if __name__ == "__main__":
     my_model = BaseModel()
